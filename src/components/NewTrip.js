f72da6b2a8ef420a0ef96b07492497b31ea0972a
max-powell <github email address>
2019-05-08 07:50:24 +0100

    changed select els to dropdown

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { Input, Form } from 'semantic-ui-react'
+import { Input, Dropdown } from 'semantic-ui-react'
 
 class NewTrip extends Component {
 
+  state = {
+    name: '',
+    location: '',
+    tripType: '',
+    month: null
+  }
+
+  handleInputChange = ({target: {name, value}}) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
   render() {
 
-    const options = [
+    {/*const options = [
       { key: 'af', text: 'Africa', value: 'Africa' },
       { key: 'as', text: 'Asia', value: 'Asia' },
       { key: 'ca', text: 'Central America', value: 'Central America' },
@@ -16,13 +29,38 @@ class NewTrip extends Component {
       { key: 'oc', text: 'Oceania', value: 'Oceania' },
       { key: 'sa', text: 'South America', value: 'South America' },
       { key: 'cb', text: 'Caribbean', value: 'Caribbean' },
+    ]*/}
+
+    const months = [
+      {name: 'January', value: 0},
+      {name: 'February', value: 1},
+      {name: 'March', value: 2},
+      {name: 'April', value: 3},
+      {name: 'May', value: 4},
+      {name: 'June', value: 5},
+      {name: 'July', value: 6},
+      {name: 'August', value: 7},
+      {name: 'September', value: 8},
+      {name: 'October', value: 9},
+      {name: 'November', value: 10},
+      {name: 'December', value: 11}
     ]
+
+    const monthOptions = months.map(m => ({
+      key: m.name,
+      text: m.name,
+      value: m.value
+    }))
+
+
 
     return (
       <div className='new-trip'>
         <h2>Start a new trip!</h2>
-        <Input fluid label='Name*' focus placeholder='Trip name...' />
-        <Form.Select fluid label='Region(optional)' options={options} placeholder='Region(optional)' />
+        <Input fluid name='name' label='Name' focus placeholder='Trip name...' onChange={this.handleInputChange} />
+        <Input fluid name='location' label='Location' focus placeholder='Where are you going?' onChange={this.handleInputChange} />
+          <Dropdown inline selection placeholder='When do you want to go?' options={monthOptions} />
+          <Dropdown inline selection placeholder='What type of trip is it?' options={monthOptions} />
       </div>
     );
   }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -11,7 +11,34 @@ const loginUrl = baseUrl + 'login'
 class MainContainer extends Component {
 
   state = {
-    currentUser: {}
+    currentUser: {
+  "id": 5,
+  "name": "Max",
+  "trips": [
+    {
+      "id": 11,
+      "name": "Temples",
+      "location": "India",
+      "month": 10,
+      "duration": 7,
+      "trip_type": "adventure",
+      "users": [
+        "Max"
+      ]
+    },
+    {
+      "id": 12,
+      "name": "Max test trip",
+      "location": "Australia",
+      "month": 7,
+      "duration": 7,
+      "trip_type": "beach",
+      "users": [
+        "Max"
+      ]
+    }
+  ]
+}
   }
 
   getCurrentUser = name => {
@@ -37,6 +64,7 @@ class MainContainer extends Component {
       <Router>
         <div className="MainContainer">
           <Route exact path='/' component={() => <Homepage signIn={getCurrentUser} validUser={validUser} />} />
+          <Route exact path='/dashboard' component={() => <Dashboard user={this.state.currentUser} />} />
         </div>
       </Router>
     );
